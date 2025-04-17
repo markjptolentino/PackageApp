70e1a05e1157c34dd89566d2a4c72781e156511a
markjptolentino <MarkJP.Tolentino@my-aolcc.com>
2025-04-17 15:47:29 -0400

    Initial commit - Shipping quote app

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Display a welcome message to the user
+        Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+        // Prompt the user to enter the package weight
+        Console.WriteLine("Please enter the package weight:");
+        double weight = Convert.ToDouble(Console.ReadLine());
+
+        // Check if the package is too heavy
+        if (weight > 50)
+        {
+            Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            return;
+        }
+
+        // Prompt the user to enter the package width
+        Console.WriteLine("Please enter the package width:");
+        double width = Convert.ToDouble(Console.ReadLine());
+
+        // Prompt the user to enter the package height
+        Console.WriteLine("Please enter the package height:");
+        double height = Convert.ToDouble(Console.ReadLine());
+
+        // Prompt the user to enter the package length
+        Console.WriteLine("Please enter the package length:");
+        double length = Convert.ToDouble(Console.ReadLine());
+
+        // Check if total dimensions exceed 50
+        if (width + height + length > 50)
+        {
+            Console.WriteLine("Package too big to be shipped via Package Express.");
+            return;
+        }
+
+        // Calculate the quote
+        double quote = (width * height * length * weight) / 100;
+
+        // Display the result formatted as currency
+        Console.WriteLine("Your estimated total for shipping this package is: $" + quote.ToString("F2"));
+        Console.WriteLine("Thank you!");
+    }
+}
